407509519dc440ec9b169ad717d9c02cb1031aa9
avtor_house@mail.ru <Gtnheirfgi88>
2022-02-15 16:58:31 +0300

    Вернул main

--- src/main/java/org/example/vtbhomework15/Main.java
+++ src/main/java/org/example/vtbhomework15/Main.java
@@ -7,5 +7,17 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println();
+
+        //  IndexStorage
+        IndexStorage storage = new IndexStorage(6);
+        System.out.println("IndexStorage, size: " + storage.size());
+        System.out.println("IndexStorage, reverse: " + Arrays.toString(storage.reverse()));
+        System.out.println();
+
+        // ArrayIndexStorage
+        ArrayIndexStorage indexStorage = new ArrayIndexStorage(new int[]{1, 5, 11, 500, -1, 0, 33});
+        System.out.println("ArrayIndexStorage, size: " + indexStorage.size());
+        System.out.println("ArrayIndexStorage, reverse: " + Arrays.toString(indexStorage.reverse()));
     }
 }
